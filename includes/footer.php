6a5866d1e1bfd0e797a729e2f154d763d64b0cf7
tsubu <tsubutoshi@gmail.com>
2026-07-03 03:19:46 +0900

    ui: フッター著作権の PICOT を GitHub リンクに変更

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,8 @@
     <footer class="site-footer">
-        <p class="site-footer__copy"><?= h(__('footer_copyright', ['year' => date('Y')])) ?></p>
+        <p class="site-footer__copy">
+            © <?= h((string) date('Y')) ?>
+            <a class="site-footer__link" href="https://github.com/tsubu/downloader" target="_blank" rel="noopener noreferrer">GitHub</a>. <?= h(__('footer_all_rights')) ?>
+        </p>
     </footer>
 </div>
 <script type="application/json" id="app-i18n-data"><?= json_encode(js_translations(), JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) ?></script>
